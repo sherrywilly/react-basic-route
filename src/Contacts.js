8b95b0676dbaea9f94d0ad6e940622e485d213a6
sherrywilly <sherrywilson521@gmail.com>
2020-09-18 09:52:35 +0530

    first commit

---
+++ src/Contacts.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Contacts extends Component {
+  render() {
+    return (
+      <div>
+        <h1>this is contacts</h1>
+      </div>
+    );
+  }
+}
